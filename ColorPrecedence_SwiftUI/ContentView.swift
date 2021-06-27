8c8f6d7e42f47db83977522b320817e7b7121c68
Theo Vora <theo.vora@gmail.com>
2021-06-26 19:31:55 -0600

    Apply color on VStack and Text within. Child wins.

--- ColorPrecedence_SwiftUI/ContentView.swift
+++ ColorPrecedence_SwiftUI/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .padding()
+            Text("Sup")
+        }
+        .foregroundColor(.red)
     }
 }
 

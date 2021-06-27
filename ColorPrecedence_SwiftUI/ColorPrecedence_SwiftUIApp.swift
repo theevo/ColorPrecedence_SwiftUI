9745c5545394e7578b1e6c13b7528cb85a9181a4
Theo Vora <theo.vora@gmail.com>
2021-06-26 19:28:08 -0600

    Initial Commit

---
+++ ColorPrecedence_SwiftUI/ColorPrecedence_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPrecedence_SwiftUIApp.swift
+//  ColorPrecedence_SwiftUI
+//
+//  Created by Theo Vora on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ColorPrecedence_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorPrecedence_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorPrecedence_SwiftUI
+//
+//  Created by Theo Vora on 6/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
